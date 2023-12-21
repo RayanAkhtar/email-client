2ad82fc1825d949b80b7a24acc112339347d8579
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2023-12-21 12:30:01 +0000

    Implemented txt file reading

--- file_reader/text_file_reader.py
+++ file_reader/text_file_reader.py
@@ -1,15 +1,33 @@
+import PyPDF2
+import docx
+
+
 #   TODO:
 #       txt file reading
 #       pdf file reading
 #       docx file reading
 
+
 def read_txt_file(file):
-    pass
+    file = open(file, 'r')
+    text = file.readlines()
+    file.close()
+    return text
 
 
 def read_pdf_file(file):
-    pass
+    txt_to_return = ""
+    file = PyPDF2.PdfReader(file)
+    num_pages = file.getNumPages()
+    for i in range(num_pages):
+        txt_to_return += file.getPage(i).extractText()
+    return txt_to_return
 
 
 def read_docx_file(file):
-    pass
+    doc = docx.Document(file)
+    txt_tp_return = ""
+    for para in doc.paragraphs:
+        txt_tp_return += para.text
+    return txt_tp_return
+
