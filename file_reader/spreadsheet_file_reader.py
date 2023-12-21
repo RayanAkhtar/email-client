d361e82f6ed690b30cf43bae1cab3085ea30ff3a
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2023-12-21 19:06:21 +0000

    Added more tests to improve versatility of spreadsheets

--- file_reader/spreadsheet_file_reader.py
+++ file_reader/spreadsheet_file_reader.py
@@ -40,10 +40,15 @@ def read_csv_file(filename):
 def read_xlsx_file(file):
     spreadsheet_file = SpreadsheetFile()
     df = pd.read_excel(file)
-    headers = df.keys().values
     records = df.values
+    headers = df.keys().values
+    diff_format = all_nans(headers)
+    if diff_format:
+        records = remove_fake_nans(records)
+        headers = records[0]
 
     for row in records:
+
         record = {}
         for i in range(len(headers)):
             val = row[i]
@@ -51,8 +56,44 @@ def read_xlsx_file(file):
                 record[headers[i]] = None
             else:
                 if isinstance(val, float):
-                    val = str(int(val))
-                record[headers[i]] = val
+                    val = int(val)
+                record[headers[i]] = str(val)
 
         spreadsheet_file.records.append(record)
+    if diff_format:
+        spreadsheet_file.records = spreadsheet_file.records[1:]
+    print(spreadsheet_file.records)
     return spreadsheet_file
+
+
+def remove_fake_nans(records):
+    fixed_records_rows = []
+    # first remove redundant rows
+    for record in records:
+        for val in record:
+            if not pd.isna(val):
+                fixed_records_rows.append(record)
+                break
+
+    # Then remove redundant columns
+    headers = []
+    for val in fixed_records_rows[0]:
+        headers.append(not pd.isna(val))  # contains true if not NaN
+
+    fixed_records = []
+    for row in fixed_records_rows:
+        record = []
+        i = 0
+        for val in row:
+            if headers[i]:
+                record.append(val)
+            i += 1
+        fixed_records.append(record)
+
+    return fixed_records
+
+def all_nans(record):
+    for val in record:
+        if 'Unnamed:' in val:
+            return True
+    return False
--- file_reader/spreadsheet_file_tests.py
+++ file_reader/spreadsheet_file_tests.py
@@ -2,6 +2,7 @@ import unittest
 import spreadsheet_file_reader as sfr
 
 spreadsheet_1 = "spreadsheet_files/excel_test_workbook"
+spreadsheet_2 = "spreadsheet_files/excel_test_workbook_2"
 
 
 class MyTestCase(unittest.TestCase):
@@ -27,8 +28,51 @@ class MyTestCase(unittest.TestCase):
         ]
         self.assertEqual(records, spreadsheet_data.records)
 
+    def test_diff_format_csv(self):
+        spreadsheet_data = sfr.read_csv_file(spreadsheet_2 + ".csv")
+        records = [
+            {
+                "name": "Rayan",
+                "age": "19",
+                "project": "Email Client"
+            },
+            {
+                "name": "Joe",
+                "age": "20",
+                "project": "Project A"
+            },
+            {
+                "name": "Smith",
+                "age": None,
+                "project": "Project B"
+            }
+        ]
+        self.assertEqual(records, spreadsheet_data.records)
+
     def test_correct_xlsx(self):
         spreadsheet_data = sfr.read_xlsx_file(spreadsheet_1 + ".xlsx")
+        records = [
+            {
+                "name": "Rayan",
+                "age": "19",
+                "project": "Email Client"
+            },
+            {
+                "name": "Joe",
+                "age": "20",
+                "project": "Project A"
+            },
+            {
+                "name": "Smith",
+                "age": None,
+                "project": "Project B"
+            }
+        ]
+        print(records)
+        self.assertEqual(records, spreadsheet_data.records)
+
+    def test_diff_format_xlsx(self):
+        spreadsheet_data = sfr.read_xlsx_file(spreadsheet_2 + ".xlsx")
         records = [
             {
                 "name": "Rayan",
