716ee47138150b05d353818080e220c518335694
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2023-12-22 14:54:09 +0000

    implemented tests, currently txt works, but need to redo pdf and docx.

--- file_writer/file_writing.py
+++ file_writer/file_writing.py
@@ -1,38 +1,26 @@
-import aspose.words as aw
-import aspose.pdf as ap
 
 
-output_path = "../output"
-
-
-def save_formatted_file(text_to_write, filename, extension):
+def save_formatted_file(text_to_write, filename, extension, output_path="../output"):
     if extension == 'txt':
-        write_to_txt(text_to_write, filename)
+        write_to_txt(text_to_write, filename, output_path)
     elif extension == 'pdf':
-        write_to_pdf(text_to_write, filename)
+        write_to_pdf(text_to_write, filename, output_path)
     elif extension == 'docx':
-        write_to_docx(text_to_write, filename)
+        write_to_docx(text_to_write, filename, output_path)
     else:
         print(f'Unsupported file: {extension}')
         exit(-1)
 
 
-def write_to_txt(text_to_write, filename):
+def write_to_txt(text_to_write, filename, output_path):
     file = open(output_path + filename + ".txt", 'w')
     file.write(text_to_write)
     file.close()
 
 
-def write_to_pdf(text_to_write, filename):
-    file = ap.Document()
-    page = file.pages.add()
-    text = ap.text.TextFragment(text_to_write)
-    page.paragraphs.add(text)
-    file.save(output_path + filename + ".pdf")
+def write_to_pdf(text_to_write, filename, output_path):
+    pass
 
 
-def write_to_docx(text_to_write, filename):
-    file = aw.Document()
-    builder = aw.DocumentBuilder(file)
-    builder.write(text_to_write)
-    file.save(output_path + filename + ".docx")
+def write_to_docx(text_to_write, filename, output_path):
+    pass
